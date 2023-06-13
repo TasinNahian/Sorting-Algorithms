ffda6e5b79533c96de1ed95ffdc918ddadd24b07
tasin nahian <tasin.nahian@gmail.com>
2023-06-13 19:54:22 +0600

    utility added

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
         int arr[] = {7,8,3,1,2};
@@ -11,14 +13,8 @@ public class BubbleSort {
                 }
             }
         }
-
-        printArray(arr);
+        Util.printArray(arr);
     }
 
-    public static void printArray(int [] arr) {
-        for (int i = 0; i< arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+
 }
---
+++ Util.java
@@ -0,0 +1,8 @@
+public class Util {
+    public static void printArray(int [] arr) {
+        for (int i = 0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
